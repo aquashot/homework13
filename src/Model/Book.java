386c21e374d5048c9af9e3e706e046d8a2e760d4
Илон <vlasovvdm@gmail.com>
2023-11-15 18:50:01 +0400

    1доделпл

--- src/Model/Book.java
+++ src/Model/Book.java
@@ -1,5 +1,7 @@
 package Model;
 
+import java.util.Objects;
+
 public class Book {
     private final String name;
     private final Author author;
@@ -26,6 +28,20 @@ public class Book {
     public void setPublicYear(int publicYear) {
         this.publicYear = publicYear;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicYear == book.publicYear && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, publicYear);
+    }
+
     @Override
     public String toString(){
         return author+ " \"" +name+ "\""+  publicYear;
